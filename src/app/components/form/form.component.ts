1ce12084e70345ebfa0bf871b9aef0d8adcd4a80
Dani Sanguino <sanguinad@gmail.com>
2025-03-21 20:01:45 +0100

    formulario definido en el ts y maquetación básica hecha. se prueba funcionalidad y todo ok

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
 export class FormComponent {
+
+  blogForm: FormGroup = new FormGroup({
+
+    titulo: new FormControl('', [
+      Validators.required
+    ]),
+    texto: new FormControl('', [
+      Validators.required
+    ]),
+    imagen: new FormControl('', [
+      Validators.required
+    ]),
+    categoria: new FormControl('', [
+      Validators.required
+    ]),
+    fecha: new FormControl('', [
+      Validators.required
+    ]),
+
+  })
+
+
+  onSubmit() {
+    console.log(this.blogForm.value)
+  }
+
 
 }
